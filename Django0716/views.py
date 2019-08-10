f6436b40446251e44a0b832540b3e72d6a5c0d38
vincenttuan <vincentjava@yahoo.com.tw>
2019-08-10 10:21:19 +0800

    Django

--- Django0716/views.py
+++ Django0716/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from math import sin, cos, sqrt, atan2, radians
-import requests
-import json
+
+from Django0716.utils import search
+
 
 def hello(request):
     return HttpResponse("Hello World")
@@ -64,35 +64,3 @@ def youbike_result(request):
     return render(request, 'youbike_result.html', {'data': dict})
 
 
-def search(mylat, mylng, dist, sbi, bemp):
-    url = 'https://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json'
-    r = requests.get(url)
-    root_object = json.loads(r.text)
-    result_object = root_object['result']
-    records_array = result_object['records']
-    dict = {}
-    for record in records_array:
-        lat = float(record['lat'])
-        lng = float(record['lng'])
-        m = distance(mylat, mylng, lat, lng)
-        t = '%.1f' % (m / (3000/60))
-        if int(record['sbi']) >= sbi and int(record['bemp']) >= bemp and m <= dist:
-            record['m'] = m//1
-            record['t'] = t
-            dict.update({record['sna']: record})
-
-    return dict
-
-def distance(point_1_lat, point_1_lon, point_2_lat, point_2_lon):
-    # approximate radius of earth in km
-    R = 6373.0
-    lat1 = radians(float(point_1_lat))
-    lon1 = radians(float(point_1_lon))
-    lat2 = radians(float(point_2_lat))
-    lon2 = radians(float(point_2_lon))
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = R * c * 1000  # m 公尺
-    return distance
